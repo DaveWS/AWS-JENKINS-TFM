aa7f059a30ff1cee89d7ed603b9c4eec9773984e
DaveWS <davidtxus@hotmail.com>
2022-09-11 01:11:42 -0500

    Update install_jenkins.sh

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -4,8 +4,8 @@
 sudo yum -y update
 
 echo "Install Java JDK 8"
-yum remove -y java
-yum install -y java-1.8.0-openjdk
+sudo yum remove -y java
+sudo yum install -y java
 
 echo "Install Maven"
 yum install -y maven 
